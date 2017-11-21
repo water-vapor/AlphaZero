584f5a6348bcb64a01eb072989b355055d61bafd
water-vapor <zzhaoah@connect.ust.hk>
2017-11-21 20:53:32 +0800

    Added GameConverter for selfplay training data (with search probabilities)

--- AlphaGoZero/reinforcement_learning/sequential/util.py
+++ AlphaGoZero/reinforcement_learning/sequential/util.py
@@ -1,3 +1,5 @@
+import os
+from AlphaGoZero.preprocessing.game_converter import GameConverter, SizeMismatchError, NoResultError
 
 def selfplay_to_h5(model_name, base_dir = 'data'):
 	""" Takes a model that has just generated the selfplay data, combine everything into a single h5 file.
@@ -7,7 +9,26 @@ def selfplay_to_h5(model_name, base_dir = 'data'):
 			model_name: name of the model
 			base_dir: the directory containing the folder selfplay.
 	"""
-	pass
+	feature_list = ["board_history", "color"]
+	converter = GameConverter(feature_list)
+	# From game converter
+	def _is_sgf(fname):
+        return fname.strip()[-4:] == ".sgf"
+	def _walk_all_sgfs(root):
+	"""a helper function/generator to get all SGF files in subdirectories of root
+	"""
+	for (dirpath, dirname, files) in os.walk(root):
+		for filename in files:
+			if _is_sgf(filename):
+				# find the corresponding pkl
+				pkl_name = filename.strip()[:-4] + '.pkl'
+				if os.path.exists(os.path.join(dirpath, pkl_name)):
+					# yield the full (relative) path to the file
+					yield os.path.join(dirpath, filename), os.path.join(dirpath, pkl_name)
+	
+	files = _walk_all_sgfs(os.path.join(base_dir, 'selfplay', model_name))
+	converter.selfplay_to_hdf5(files, os.path.join(base_dir, 'selfplay', model_name, 'train.h5'), 19)
+	
 	
 def get_current_time():
     return '_'.join(re.findall('\d+', str(np.datetime64('now'))))
@@ -17,3 +38,6 @@ def sgf_to_h5(path_to_sgf, destination_path, filename):
     args = ['--outfile', h5path, '--directory', path_to_sgf]
     run_game_converter(args)
     return h5path
+	
+def combined_train_data_generator(h5_files, num_batch):
+	pass
