e4d59e2c8fafc6505a91af980cd7f17bd9091e46
water-vapor <zzhaoah@connect.ust.hk>
2017-11-21 15:38:37 +0800

    redesigned file structure, updated RL logic

--- AlphaGoZero/reinforcement_learning/sequential/reinforcement.py
+++ AlphaGoZero/reinforcement_learning/sequential/reinforcement.py
@@ -1,48 +1,83 @@
-import argparse, re
+import argparse, re, os, json
 import numpy as np
 from AlphaGoZero.Network.main import Network
 from AlphaGoZero.reinforcement_learning.sequential.optimization import optimize, sgf_to_h5
 from AlphaGoZero.reinforcement_learning.sequential.evaluator import evaluate
 from AlphaGoZero.reinforcement_learning.sequential.selfplay import selfplay
-
-
-def get_current_time():
-    return '_'.join(re.findall('\d+', str(np.datetime64('now'))))
-
+from AlphaGoZero.reinforcement_learning.sequential.util import selfplay_to_h5, get_current_time
 
 def main():
+	""" File structure: The program uses 'data/models', 'data/selfplay/' and 'data/evaluations' as defaults.
+		data/models contains the folders of tensorflow models, where the model names are time strings
+		data/selfplay contains folders, who was once the best player. The folders contains sgf files ,pkl files generated from the selfplay.
+		data/evaluations cntains evaluation matches, whose names are concatenated from the player's name
+		
+		Metainfo are stored in data/config.json, please do not delete if it exists.
+		Format: A dictionary with keys:
+		'models': a list of strings of model names, ascending order in time
+		'best_model': the name of the best model 
+		'selfplay': a list of model names that has selfplay data available in the selfplay folder, ascending order in time
+		'should_selfplay': a bool indicating whether the selfplay data of the current best model is completed, False if it is.
+		
+	"""
     parser = argparse.ArgumentParser(description='Reinforcement Learning trainer')
-    parser.add_argument("--model", "-m", help='Path to existing best model, starts from scratch if not specified',
-                        default=None)
-    parser.add_argument("--directory", "-d", help="Folder to store generated models and data", default=None)
+    parser.add_argument("--directory", "-d", help="Folder to store generated models and data", default='data')
+	parser.add_argument("--iteration", "-i", help="Number of steps to train the NN", default=100)
     args = parser.parse_args()
-    if args.model is None:
-        random_model = Network()
-        best = get_current_time()
-        random_model.save(best)
-    else:
-        best = args.model
-
-    best_defeated = True
-    new_model = best
-    # Store best models self-play data in a sequential order, since optimization requires 500,000/25,000 recent
-    # self-play games
-    best_models_data = []
-
-    # The evolution logic is not clearly described in the paper.
-    # Assuming the training happens sequentially, always optimize the newest model
-    while True:
-        if best_defeated:
-            best = new_model
-            selfplay(best, best + '_data')
-            sgf_to_h5(best + '_data', best + '_data', 'train.h5')
-            best_models_data.append(best + '_data')
-        prev_model = new_model
-        new_model = get_current_time()
-        # Optimize with recently generated self play data, which is in a list of folders
-        optimize(best_models_data[- min(20, len(best_models)):], prev_model, new_model)
-        best_defeated = evaluate(best, new_model)
-
+	
+	# Check if there is data in the location
+	if os.path.exists(os.path.join(args.directory, 'models')) and os.path.exists(os.path.join(args.directory, 'selfplay')):
+		# Check configuration file
+		assert os.path.isfile(os.path.join(args.directory, 'config.json')), 'Files found but config is missing, please consider add one manaully'
+		with open(os.path.join(args.directory, 'config.json')) as data:
+			rl_info = json.load(data)
+	else:
+		# Initialize models from scratch
+		
+		# Create necessary folders
+		if not os.path.exists(args.directory):
+			os.makedirs(args.directory)
+		os.makedirs(os.path.join(args.directory, 'models'))
+		os.makedirs(os.path.join(args.directory, 'selfplay'))
+		
+		# Create config dictionary
+		rl_info = {}
+		rl_info['models'] = []
+		rl_info['selfplay'] = []
+		rl_info['should_selfplay'] = True
+		
+		# Create the first random model 
+		random_model = Network()
+		random_model_name = get_current_time()
+		random_model.save(os.path.join(args.directory, 'models', random_model_name))
+		# Since this is the only model, it is the best
+		rl_info['models'].append(random_model_name)
+		rl_info['best_model'] = random_model_name
+		
+	# Evaluation folder is not important
+	if not os.path.exists(os.path.join(args.directory, 'evaluations')):
+		os.makedirs(os.path.join(args.directory, 'evaluations'))
+		
+		
+	# Main Pipline loop
+	for t in range(args.iteration):
+		if rl_info['should_selfplay']:
+			# Generate selfplay data and save to a h5 file
+			selfplay(rl_info['best_model'], base_dir=args.directory)
+			selfplay_to_h5(rl_info['best_model'], base_dir=args.directory)
+			rl_info['selfplay'].append(rl_info['best_model')
+			rl_info['should_selfplay'] = False
+		
+		# Use the recent (several) best selfplay data to optimize
+		# Optimize takes a list of model names and loads the most recent model. It returns the newly created model.
+		new_model_name = optimize(rl['selfplay'][-25:], rl_info['models'][-1], base_dir=args.directory)
+		rl_info['models'].append(new_model_name)
+		
+		# Evaluate the newly created model with the current best one
+		best_defeated = evaluate(rl_info['best'], new_model_name)
+		if best_defeated:
+			rl_info['best_model'] = new_model_name
+			rl_info['should_selfplay'] = True
 
 if __name__ == '__main__':
     main()
---
+++ AlphaGoZero/reinforcement_learning/sequential/util.py
@@ -0,0 +1,10 @@
+
+def selfplay_to_h5(model_name, base_dir = 'data'):
+	""" Takes a model that has just generated the selfplay data, combine everything into a single h5 file.
+		And store the h5 file as 'train.h5' in the same folder.
+		
+		Arguments:
+			model_name: name of the model
+			base_dir: the directory containing the folder selfplay.
+	"""
+	pass
