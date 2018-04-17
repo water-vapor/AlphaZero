8e0a9f882351e09494a6ce88d63e8989bb7fa2ac
water-vapor <zzhaoah@connect.ust.hk>
2018-04-17 17:55:52 +0800

    fixed name shadowing

--- AlphaZero/gtp.py
+++ AlphaZero/gtp.py
@@ -12,16 +12,20 @@ with open('AlphaZero/config/gtp.yaml') as f:
 _state_tensor_converter = _preproc.StateTensorConverter(game_config)
 _tensor_action_converter = _preproc.TensorActionConverter(game_config)
 
-net = network.Network(game_config, config_file='AlphaZero/config/gtp.yaml', pretrained=ext_config['pretrained'], mode='NCHW')
+net = network.Network(game_config, config_file='AlphaZero/config/gtp.yaml', pretrained=ext_config['pretrained'],
+                      mode='NCHW')
 
-def eval(state):
+
+def nn_eval(state):
     state_np = _state_tensor_converter.state_to_tensor(state)
     result_np = net.response(np.expand_dims(state_np, 0))
     return _tensor_action_converter.tensor_to_action(result_np[0][0]), result_np[1][0]
 
+
 def get_move(state):
-    mcts = MCTS.MCTSearch(eval, game_config, max_playout=ext_config['max_playout'])
+    mcts = MCTS.MCTSearch(nn_eval, game_config, max_playout=ext_config['max_playout'])
     move, _ = mcts.calc_move_with_probs(state)
     return move
+
 
 gtp_wrapper.run_gtp(get_move)
