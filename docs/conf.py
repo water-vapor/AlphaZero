920162071c7a1557cbf45ffdecd840ee2b25b88f
water-vapor <zzhaoah@connect.ust.hk>
2018-06-05 17:23:47 +0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+from unittest.mock import MagicMock
 
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -154,3 +155,15 @@ texinfo_documents = [
 
 # -- Extension configuration -------------------------------------------------
 autodoc_member_order = 'bysource'
+
+
+# Mock tensorflow module
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ['tensorflow']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
